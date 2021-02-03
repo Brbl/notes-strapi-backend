632257b06151d9fd7234c6b461f19ab673577121
simon.marcelle <s.marcelle@groupeonepoint.com>
2021-02-03 10:36:42 +0100

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e61560b14292beea09265b3ed5733c85'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c2158930-b199-400a-83ab-655ad6cbadee'
+};
