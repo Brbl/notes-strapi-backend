e571c096de3d79a2f6f2cc403de2bee1a2ec83ed
simon.marcelle <s.marcelle@groupeonepoint.com>
2021-02-03 12:08:19 +0100

    Add Notes

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -4,17 +4,17 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: env('DATABASE_URI')
-        //host: env('DATABASE_HOST', 'strapi-cluster.k84ut.mongodb.net'),
-        //srv: env.bool('DATABASE_SRV', true),
-        //port: env.int('DATABASE_PORT', 27017),
-        //database: env('DATABASE_NAME', 'Strapi-Backend'),
-        //username: env('DATABASE_USERNAME', 'strapi'),
-        //password: env('DATABASE_PASSWORD', 'VcJrXS1HuNvDLOZ7'),
+        //uri: env('DATABASE_URI')
+        host: env('DATABASE_HOST', 'strapi-cluster.k84ut.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'Strapi-Backend'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'VcJrXS1HuNvDLOZ7'),
       },
       options: {
-        //authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: true //env.bool('DATABASE_SSL', true),
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
